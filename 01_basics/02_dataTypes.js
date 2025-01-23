cd863cebe5c7b169337c73c638a6a2a2ef12edb8
crakecodeswithstuti <singhstuti409@gmail.com>
2025-01-24 01:39:55 +0530

    message

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+ "use strict";// treat all JS  code as newer version 
+
+//alert("3 + 3") //we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high 
+
+console.log("stuti")
+
+let name = "stuti"
+let age = 20
+let isLKoggedIn = false
+
+
+
+// number => 2 to power 53
+// bigint 
+// string =>""
+// boolean => true/false
+// null => standlone value
+// undefined =>
+// symbol => unique
+
+
+// object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+
+
