d467cbdebb0473bd109ad98c37947bdbd9e8cfd8
crakecodeswithstuti <singhstuti409@gmail.com>
2025-09-18 14:51:02 +0530

    Message

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -13,7 +13,7 @@ let age = 20
 let isLKoggedIn = false
 
 
-
+// primitive data type /
 // number => 2 to power 53
 // bigint 
 // string =>""
@@ -21,12 +21,37 @@ let isLKoggedIn = false
 // null => standlone value
 // undefined =>
 // symbol => unique
+//bigInt 
+// reference data type/non primitive data type / complex data type
+// array 
+// function
+// object  
 
+// Javascript is a dynamically typed language
+//typescript is a statically typed language 
+const score:number = 100 // number define
+const scoreValue = 100.3 // number define
+const isLoggedIn = false // boolean
+const outsideTemp = null // null
+let userEmail; // undefined
 
-// object 
 
 console.log(typeof undefined); // undefined
 console.log(typeof null); // object
+console.log(typeof 33); // number
+console.log(typeof 3.4); // number
+console.log(typeof "stuti");// string
+console.log(typeof true);// boolean
+console.log(typeof BigInt(33)); //bigint
+console.log(typeof Symbol("stuti")); // symbol
+console.log(typeof [1,2,3,4]); // object
+console.log(typeof {name:"stuti", age:20}); // object
+console.log(typeof function(){}); // function
+
+// JS is a case sensitive language
+
+
+
 
 
 
